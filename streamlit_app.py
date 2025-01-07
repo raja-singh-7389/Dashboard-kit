c2d475145da2cdc479ed46c87af405af52ec8063
Raja Singh Thakur <raja1011singhrajput@gmail.com>
2025-01-07 10:35:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 
-# Load the image
-try:
-    image = Image.open("a9109b00-26b3-4532-852f-169e62861ac8.jpg")
-except FileNotFoundError:
-    st.warning("Image not found. Using placeholder image.")
-    image = Image.open("https://via.placeholder.com/300")  # Placeholder image URL
+image = Image(a9109b00-26b3-4532-852f-169e62861ac8.jpg.JPG)
 
 # Page Configuration
 st.set_page_config(layout="wide", page_title="Portfolio - Raja Singh Thakur")
