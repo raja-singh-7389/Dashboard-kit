9d8f85b38f9c5aa42f0266354825dc2c1308c324
Raja Singh Thakur <raja1011singhrajput@gmail.com>
2025-01-07 11:06:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 
-image = Image("Image.jpg")
+# Open the image using Image.open() method
+image = Image.open("Image.jpg")
 
 # Page Configuration
 st.set_page_config(layout="wide", page_title="Portfolio - Raja Singh Thakur")
