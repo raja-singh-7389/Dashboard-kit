a022864073906649c01d91800e0907862757855b
Raja Singh Thakur <raja1011singhrajput@gmail.com>
2025-01-07 08:16:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ st.title('My Portfolio website')
 st.subheader('Welcome')
 
 
-st.write('''
-Certifications
+st.write('''Certifications
 
-Advance probability theory - IIT Delhi
-Applied Linear Algebra in AI and ML - IIT Kharagpur
-Data Analytics with Python - IIT Roorkee
-Database Management System - IIT Kharagpur
-Business Statistics - IIT Roorkee
-Data structures And Algorithms using Python - Chennai Mathematical Institute
-Enhacing soft skills and Personlity - IIT Kanpur
+- Advance probability theory - IIT Delhi
+- Applied Linear Algebra in AI and ML - IIT Kharagpur
+- Data Analytics with Python - IIT Roorkee
+- Database Management System - IIT Kharagpur
+- Business Statistics - IIT Roorkee
+- Data structures And Algorithms using Python - Chennai Mathematical Institute
+- Enhacing soft skills and Personlity - IIT Kanpur
+
 ''')
 
