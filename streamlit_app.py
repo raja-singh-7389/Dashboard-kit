15675f10d349165b3fe210786785fe6660e2a117
Raja Singh Thakur <raja1011singhrajput@gmail.com>
2025-01-07 08:23:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,11 @@ st.set_page_config(layout="wide")
 st.title('My Portfolio website')
 st.subheader('Welcome')
 
-st.info("About Me
+st.info("
+About Me
 
-        Hi! I’m Raja Singh Thakur, I am a passionate data scientist and analyst with a focus on turning data into actionable insights. With expertise in data visualization, statistical analysis, and machine learning, I enjoy solving complex problems and helping businesses make informed decisions. My goal is to uncover stories hidden within data and deliver clear, impactful solutions.")
+Hi! I’m Raja Singh Thakur, I am a passionate data scientist and analyst with a focus on turning data into actionable insights. With expertise in data visualization, statistical analysis, and machine learning, I enjoy solving complex problems and helping businesses make informed decisions. My goal is to uncover stories hidden within data and deliver clear, impactful solutions.
+       ")
 
 st.write('''Certifications
 
@@ -18,5 +20,5 @@ st.write('''Certifications
 - Enhacing soft skills and Personlity - IIT Kanpur
 
 ''')
-st.write("[View certificate]()")
+st.write("[View certificate](https://github.com/raja-singh-7389/Portfolio-website.git)")
 
